1d2a8df067bf4fc630ac76f39ad9fdba1278d107
Arthur Rosch <rosch.arthur@gmail.com>
2022-08-31 12:40:24 -0300

    Exec Finalizado

---
+++ app.js
@@ -0,0 +1,34 @@
+var nome,
+  escolha,
+  salario = 0,
+  salarioNovo = 0,
+  aumento = 0;
+
+Dados();
+SalarioComparador(salario);
+salarioNovo = Aumento(salario, aumento);
+console.log(`${nome}\n R$ ${salario} \n${aumento}% \n${salarioNovo} `);
+continuar();
+
+function Dados() {
+  nome = prompt("Qual o seu nome: ");
+  salario = parseFloat(prompt("Qual o seu Salario"));
+}
+function continuar() {
+  escolha = prompt("1-Para continuar\n2-Para Parar");
+  return;
+}
+function SalarioComparador(salario) {
+  if (salario < 1500) {
+    aumento = 20;
+  } else if (salario > 1500 && salario < 2000) {
+    aumento = 15;
+  } else if (salario > 2000 && salario < 3000) {
+    aumento = 10;
+  } else if (salario > 3000) {
+    aumento = 5;
+  }
+}
+function Aumento(salario, aumento) {
+  return salario + salario * (aumento / 100);
+}
